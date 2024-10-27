a706ced5d26671749e19afb03341fe60d967d7d6
Kisore <kisore.s70@gmail.com>
2024-10-27 23:07:46 +0530

    set up apollo server and prisma client

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+import { createContext } from "./context";
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    context: async (request) => createContext(request),
+  });
+  console.log(`Server is running at ${url}`);
+}
+
+startApolloServer();
